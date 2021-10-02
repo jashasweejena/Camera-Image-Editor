9564be43f888025fa0bce89128db9e2e842d03b6
Jashaswee Jena <j.ishujena2000@gmail.com>
2021-10-03 02:01:27 +0530

    ImageEditorFragment: Add rotation feature with animation

--- app/src/main/java/com/example/image_editor_assignment/fragments/ImageEditorFragment.kt
+++ app/src/main/java/com/example/image_editor_assignment/fragments/ImageEditorFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.image_editor_assignment.databinding.FragmentImageEditorBinding
+
+import android.animation.Animator
+
 
 class ImageEditorFragment : Fragment() {
     val args: ImageEditorFragmentArgs by navArgs()
@@ -25,6 +29,39 @@ class ImageEditorFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         imageEditorBinding?.imageView?.let { Glide.with(it.context).load(args.imageUri).into(it) }
+        imageEditorBinding?.rotateBtn?.setOnClickListener {
+            rotateImage(-90f, imageEditorBinding?.imageView)
+        }
+    }
+
+    private fun rotateImage(angle: Float, imageView: ImageView?) {
+
+        imageView?.let { imageView ->
+            val h = imageView.height
+            val w = imageView.width
+
+            val scaleFactor: Float =
+                if (imageView.rotation / angle % 2 == 0f) (w * 1.0f / h) else 1f
+
+            imageView.animate()?.rotationBy(angle)?.scaleX(scaleFactor)?.scaleY(scaleFactor)
+                ?.setListener(
+                    object : Animator.AnimatorListener {
+                        override fun onAnimationStart(p0: Animator?) {
+                            imageEditorBinding?.rotateBtn?.isEnabled = false
+                        }
+
+                        override fun onAnimationEnd(p0: Animator?) {
+                            imageEditorBinding?.rotateBtn?.isEnabled = true
+                        }
+
+                        override fun onAnimationCancel(p0: Animator?) = Unit
+
+                        override fun onAnimationRepeat(p0: Animator?) = Unit
+
+                    })
+        }
+
+
     }
 
     companion object {
